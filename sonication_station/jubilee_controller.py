de2e0edb222cf2f9ecd9d93cb107836ef5e00f08
poofjunior <seeifyoucanmeetme@gmail.com>
2020-08-26 20:54:40 +0100

    http-only interface seems to be working

--- sonication_station/jubilee_controller.py
+++ sonication_station/jubilee_controller.py
@@ -6,7 +6,6 @@ import json
 import time
 import curses
 import pprint
-from threading import Thread, Lock
 from inpromptu import Inpromptu, cli_method
 from functools import wraps
 
@@ -19,7 +18,8 @@ class MachineStateError(Exception):
 def machine_is_homed(func):
     @wraps(func) # We need this for @cli_method to work
     def homing_check(self, *args, **kwds):
-        if not all([axis['homed'] for axis in self.machine_model['move']['axes']]):
+        axes_homed = json.loads(self.gcode("M409 K\"move.axes[].homed\""))["result"]
+        if not all(axes_homed):
             raise MachineStateError("Error: machine must first be homed.")
         return func(self, *args, **kwds)
 
@@ -46,8 +46,6 @@ class JubileeMotionController(Inpromptu):
         self.model_update_timestamp = 0
         self.command_ws = None
         self.wake_time = None # Next scheduled time that the update thread updates.
-        self.state_update_thread = None # The thread.
-        self.keep_subscribing = True # bool for keeping the thread alive.
         self.absolute_moves = True
         self.connect()
         if reset:
@@ -59,109 +57,7 @@ class JubileeMotionController(Inpromptu):
         """Connect to Jubilee over the default unix socket."""
         if self.simulated:
             return
-        # Launch the Update Thread.
-        self.state_update_thread = \
-            Thread(target=self.update_machine_model_worker,
-                    name="Machine Model Update Thread",
-                    daemon=True)
-        self.state_update_thread.start()
-
-
-    def update_machine_model_worker(self):
-        """Thread worker for periodically updating the machine model."""
-        if self.simulated:
-            return
-
-        # Subscribe to machine model updates
-        subscribe_ws = websocket.create_connection(f"ws://{self.address}/machine")
-        # Set the wakeup schedule based on the first time we update.
-        self.wake_time = time.perf_counter()
-        # Do the first update.
-        with Lock(): # Lock access to the machine model before updating it.
-            self.machine_model = json.loads(subscribe_ws.recv())
-        pprint.pprint(self.machine_model)
-        self.model_update_timestamp = time.perf_counter()
-        # Do scheduled updates on a loop.
-        while self.keep_subscribing:
-            #print(f"thread woke up at {time.perf_counter()}")
-            if self.debug:
-                loop_start = time.perf_counter()
-            # Acknowledge patch and request more; apply the update
-            subscribe_ws.send("OK\n")
-            start_time = time.perf_counter()
-            machine_model_patch = json.loads(subscribe_ws.recv())
-            if self.debug:
-                pprint.pprint(machine_model_patch)
-            self.apply_patch(machine_model_patch)
-            self.model_update_timestamp = time.perf_counter()
-            # TODO: only update if there is actual data.
-            with Lock(): # Lock access to the machine model.
-                self.machine_model.update(machine_model_patch)
-            #if self.debug:
-            #    print(f"lock + receive delay: {time.perf_counter() - start_time}")
-            # Sleep until next scheduled update time.
-            #if self.debug:
-            #    print(f"loop time: {time.perf_counter() - loop_start}")
-            #    print()
-            # Update the next wake time.
-            self.wake_time = self.__class__.POLL_INTERVAL_S + self.wake_time
-            if time.perf_counter() <= self.wake_time:
-                #print(f"thread sleeping. next update time: {self.wake_time}")
-                time.sleep(self.wake_time - time.perf_counter())
-            else:
-                print("Error: thread update speed too fast! Missed update deadline!")
-
-        subscribe_ws.close()
-
-
-    def apply_patch(self, patch):
-        return self._apply_patch(patch, self.machine_model)
-
-    def _apply_patch(self, patch, object_model_ref=None, indentation=0):
-        """recursively apply object model patch to the current object model."""
-        # Starting case.
-        #if indentation == 0:
-        #    print("APPLYING PATCH")
-        #    print(" "*indentation + f"current_object_model: original.")
-        #else:
-        #    print(" "*indentation + f"current_object_model: {object_model_ref}")
-
-        if type(patch) == list:
-            #print(" "*indentation + f"current_patch (list): {patch}")
-            # Check if item was removed from list. If so, take the whole patch.
-            if len(patch) < len(object_model_ref):
-                # Modify by reference, not by value
-                object_model_ref.clear()
-                object_model_ref.extend(patch)
-                return
-            for index, patch_item in enumerate(patch):
-                # Add new entries to the list.
-                #print(" "*indentation + f"list index: {index} | len(object_model_ref): {len(object_model_ref)}")
-                if index >= len(object_model_ref):
-                    #print(" "*indentation + f"APPENDING {patch_item} to {object_model_ref}")
-                    object_model_ref.append(patch_item)
-                    #print(" "*indentation + f"RESULT: {object_model_ref}")
-                elif type(patch_item) in [dict, list]:
-                    self._apply_patch(patch_item, object_model_ref[index], indentation+4)
-                else:
-                    # Base case.
-                    object_model_ref[index] = patch_item
-        elif type(patch) == dict:
-            #print(" "*indentation + f"current_patch (dict): {patch}")
-            for patch_key in patch.keys():
-                if type(patch[patch_key]) == dict:
-                    # Add new entries to the dict
-                    if patch_key not in object_model_ref:
-                        object_model_ref[patch_key] = patch[patch_key]
-                    else:
-                        self._apply_patch(patch[patch_key], object_model_ref[patch_key], indentation+4)
-                elif type(patch[patch_key]) == list:
-                    self._apply_patch(patch[patch_key], object_model_ref[patch_key], indentation+4)
-                else:
-                    # Base case.
-                    object_model_ref[patch_key] = patch[patch_key]
-        #print(" "*indentation + f"done")
-
+        # TODO: maybe do a ping here?
 
 
     def gcode(self, cmd: str = ""):
@@ -199,7 +95,10 @@ class JubileeMotionController(Inpromptu):
 
     @cli_method
     def home_all(self):
-        self.gcode("M98 P\"homeall.g\"")
+        # Having a tool is only possible if the machine was already homed.
+        if self.active_tool_index != -1:
+            self.park_tool()
+        self.gcode("G28")
         self._set_absolute_moves(force=True)
 
 
@@ -229,7 +128,7 @@ class JubileeMotionController(Inpromptu):
     def home_in_place(self, *args: str):
         """Set the current location of a machine axis or axes to 0."""
         for axis in args:
-            if axis not in ['X', 'Y', 'Z', 'U']:
+            if axis.upper() not in ['X', 'Y', 'Z', 'U']:
                 raise TypeError(f"Error: cannot home unknown axis: {axis}.")
             self.gcode(f"G92 {axis.upper()}0")
 
@@ -242,7 +141,8 @@ class JubileeMotionController(Inpromptu):
         x_movement = f"X{x} " if x is not None else ""
         y_movement = f"Y{y} " if y is not None else ""
         z_movement = f"Z{z} " if z is not None else ""
-        self.gcode(f"G0 {x_movement}{y_movement}{z_movement}F13000")
+        if x_movement or y_movement or z_movement:
+            self.gcode(f"G0 {x_movement}{y_movement}{z_movement}F13000")
         if wait:
             self.gcode(f"M400")
 
@@ -278,25 +178,11 @@ class JubileeMotionController(Inpromptu):
     @cli_method
     def position(self):
         """Returns the machine control point in mm."""
-        # TODO: consider replacing with M114.
-        # We are assuming axes are ordered X, Y, Z, U.
-        tool_offsets = [0, 0, 0]
-        if self.active_tool_index != -1: # "-1" is equivalent to "no tools."
-            tool_offsets = self.machine_model['tools'][self.active_tool_index]['offsets'][:3]
+        # Axes are ordered X, Y, Z, U, E, E0, E1, ... En, where E is a copy of E0.
+        response_chunks = self.gcode("M114").split()
+        positions = [float(a.split(":")[1]) for a in response_chunks[:3]]
+        return positions
 
-        axis_info = self.machine_model['move']['axes']
-        x = axis_info[0].get('machinePosition', None)
-        y = axis_info[1].get('machinePosition', None)
-        z = axis_info[2].get('machinePosition', None)
-
-        if x is not None:
-            x += tool_offsets[0]
-        if y is not None:
-            y += tool_offsets[1]
-        if z is not None:
-            z += tool_offsets[2]
-
-        return x, y, z
 
 
     @cli_method
@@ -325,7 +211,7 @@ class JubileeMotionController(Inpromptu):
     def active_tool_index(self):
         """Return the index of the current tool."""
         # TODO: consider replacing with T.
-        return self.machine_model['state']['currentTool']
+        return int(self.gcode("T"))
 
 
     @cli_method
@@ -391,7 +277,7 @@ class JubileeMotionController(Inpromptu):
                     if step_size > max_step_size:
                         step_size = max_step_size
                     stdscr.addstr(7,0,f"Step Size: {step_size:<8}")
-            self.wait_until_idle()
+            self.move_xyz_absolute(wait=True)
         finally:
             curses.nocbreak()
             stdscr.keypad(False)
@@ -400,42 +286,10 @@ class JubileeMotionController(Inpromptu):
             self._set_absolute_moves(force=True)
 
 
-    def wait_until_idle(self, timeout = TIMEOUT_S):
-        start_wait_time = time.perf_counter()
-        # Wait at least a full update interval to ensure we are polling
-        # new data after the move command was sent.
-        # Note: we are assuming that if a gcode is acknowledged it immediately
-        #       changes from idle to busy if it was not already busy.
-        #print(f"start state: {self.machine_model['state']['status'].lower()} | {time.perf_counter():.3f}")
-        self._sleep_until_next_update()
-        while self.is_busy:
-            #print(f"curr state:  {self.machine_model['state']['status'].lower()} | {time.perf_counter():.3f}")
-            if time.perf_counter() - start_wait_time > timeout:
-                raise RuntimeError("Error: Machine has timed out while waiting for a move to complete.")
-            self._sleep_until_next_update()
-        #print(f"end state:   {self.machine_model['state']['status'].lower()} | {time.perf_counter():.3f}")
-
-
-    def _sleep_until_next_update(self):
-        """Sleep until we know the machine model has received fresh data."""
-        last_model_update = self.model_update_timestamp
-        # Sleep at least until the thread is scheduled to update again.
-        #print(f"attempting to sleep at {time.perf_counter()}. Will sleep till {self.wake_time}")
-        sleep_interval = self.wake_time - time.perf_counter()
-        if sleep_interval < 0:
-            # Woke up before or during update thread's execution. Sleep again.
-            #print(f"  Awoke too early. Will actually sleep till {self.wake_time + self.__class__.POLL_INTERVAL_S}")
-            sleep_interval = self.wake_time + self.__class__.POLL_INTERVAL_S - time.perf_counter()
-        time.sleep(sleep_interval)
-        # Sleep in small increments until the thread has finished the current update.
-        while self.model_update_timestamp == last_model_update:
-            time.sleep(0.001)
-
-
     def disconnect(self):
         """Close the connection."""
-        if not self.simulated:
-            self.command_ws.close()
+        # Nothing to do?
+        pass
 
 
     def __enter__(self):
